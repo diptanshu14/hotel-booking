b6863ba0dd1df43203f2c7e7feb3c30335e8ba8a
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-08-02 17:28:38 +0530

    layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,15 @@
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
+import Layout from "./layouts/Layout"
 
 const App = () => {
   return (
-    <div className="bg-red-500">App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout>Home Page</Layout>} />
+        <Route path="/search" element={<Layout>Search Page</Layout>} />
+        <Route path="*" element={<Navigate to="/" />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
