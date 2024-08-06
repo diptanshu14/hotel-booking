d125e57ea03550c6dac025ae64bf357483924cb1
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-08-06 18:08:50 +0530

    user registration ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom"
 import Layout from "./layouts/Layout"
+import Register from "./pages/Register"
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout>Home Page</Layout>} />
         <Route path="/search" element={<Layout>Search Page</Layout>} />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="*" element={<Navigate to="/" />}/>
       </Routes>
     </BrowserRouter>
