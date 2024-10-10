16e868666f5b378d3e1f5234542cfa9bac1ae096
diptanshu14 <chattopadhyaydiptanshu@gmail.com>
2024-10-10 11:27:29 +0530

    user registration page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Layout from "./layouts/Layout"
+import Register from "./pages/Register"
 
 const App = () => {
   return (
@@ -7,6 +8,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout>Home</Layout>} />
         <Route path="/search" element={<Layout>Search</Layout>} />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,10 @@ mongoose.connect(process.env.DB_CONNECTION_URI as string).then(() => {
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
 
 app.use(routes)
 
